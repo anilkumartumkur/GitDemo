8b9577810ffc6b850234d877f1fafcf7dccd9978
anilkumartumkur <aniltc1146@gmail.com>
2025-03-18 20:46:40 +0530

    Architect Asian Development

---
+++ src/test/java/RealTimeProject/EcommmerseProject/Test/Demo.java
@@ -0,0 +1,9 @@
+package RealTimeProject.EcommmerseProject.Test;
+
+public class Demo {
+public static void main(String[] args) {
+	int x=10;
+	int y=20;
+	System.out.println(x+y+"sum of two numbers");
+}
+}
